37d5340907d9c775f8f3896bffbac46f1e62e1e8
xiaoqi <xiaoqi.ma@tuya.com>
2025-08-14 10:58:20 +0800

    ci: 💻 update CI

--- build/updatePackage.js
+++ build/updatePackage.js
@@ -2,7 +2,7 @@
  * @Author: mjh
  * @Date: 2025-08-14 10:42:36
  * @LastEditors: mjh
- * @LastEditTime: 2025-08-14 10:54:31
+ * @LastEditTime: 2025-08-14 10:58:11
  * @Description:
  */
 const fs = require('fs');
@@ -10,20 +10,24 @@ const path = require('path');
 const { execSync } = require('child_process');
 
 const updatePackage = async () => {
-  const miniapp_package_path = path.join(__dirname, `../miniapp-smart-ui/package.json`);
-  const miniappPackageData = fs.readFileSync(miniapp_package_path, 'utf8');
-  const miniappJsonData = miniappPackageData ? JSON.parse(miniappPackageData) : {};
-  const { version } = miniappJsonData;
+  try {
+    const miniapp_package_path = path.join(__dirname, `../miniapp-smart-ui/package.json`);
+    const miniappPackageData = fs.readFileSync(miniapp_package_path, 'utf8');
+    const miniappJsonData = miniappPackageData ? JSON.parse(miniappPackageData) : {};
+    const { version } = miniappJsonData;
 
-  const ray_package_path = path.join(__dirname, `../package.json`);
-  const rayPackageStr = fs.readFileSync(ray_package_path, 'utf8');
-  const rayPackageData = rayPackageStr ? JSON.parse(rayPackageStr) : {};
-  rayPackageData.dependencies['@tuya-miniapp/smart-ui'] = version.includes('beta')
-    ? version
-    : `^${version}`;
-  fs.writeFileSync(ray_package_path, JSON.stringify(rayPackageData, null, 2), 'utf-8');
-  console.log(version, '--update version');
-  await execSync('yarn');
+    const ray_package_path = path.join(__dirname, `../package.json`);
+    const rayPackageStr = fs.readFileSync(ray_package_path, 'utf8');
+    const rayPackageData = rayPackageStr ? JSON.parse(rayPackageStr) : {};
+    rayPackageData.dependencies['@tuya-miniapp/smart-ui'] = version.includes('beta')
+      ? version
+      : `^${version}`;
+    fs.writeFileSync(ray_package_path, JSON.stringify(rayPackageData, null, 2), 'utf-8');
+    console.log(version, '--update version');
+    await execSync('yarn');
+  } catch (err) {
+    console.error('Error updating package:', err);
+  }
 };
 
 module.exports = updatePackage;
