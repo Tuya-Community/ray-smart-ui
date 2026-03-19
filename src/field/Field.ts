6e95fbd7585783228027ba0b2ddb2397a2298e89
xiaoqi <xiaoqi.ma@tuya.com>
2026-03-19 17:37:54 +0800

    feat: add number formatting utilities to Field module

--- src/field/Field.ts
+++ src/field/Field.ts
@@ -1,4 +1,9 @@
 // @ts-ignore
 import Field from '@tuya-miniapp/smart-ui/dist/field/index';
+export {
+  getNumberFormatConfig,
+  parseFormattedNumber,
+  formatNumber,
+} from '@tuya-miniapp/smart-ui/dist/field/numberFormat';
 
 export default Field;
